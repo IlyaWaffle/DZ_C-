b8181849a3d7f9e57c6e129d3fe3db0cd6670a8d
IlyaPC <krotov891@gmail.com>
2023-03-10 21:49:31 +0300

    dz2

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
-﻿
+﻿#pragma warning disable
 /*       **Задача 16:**
 Напишите программу, которая принимает на вход два числа и проверяет, является ли первое число квадратом второго.    */
 
-Console.Write($"Hello");
-Console.Write("Hello");
+int num;
+Console.Write("Введите число: ");
+int.TryParse(Console.ReadLine(), out num);
+if (num % 7 == 0 && num % 23 == 0)
+    Console.WriteLine("Да");
+else
+    Console.WriteLine("Нет");
+
